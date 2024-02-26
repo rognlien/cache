c8a3df758d4ea1fe03d84ba2d9517fb0a4282119
Bendik Rognlien Johansen <bendik@me.com>
2024-02-26 12:11:04 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,8 +68,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/rognlien/cache")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
